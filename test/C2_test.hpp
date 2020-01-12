a599c4e59e0130f237bed144122d1ef80a9aee37
cuiy0006 <yc2554@nyu.edu>
2020-01-12 04:09:16 -0500

    test case

--- test/C2_test.hpp
+++ test/C2_test.hpp
@@ -1,7 +1,15 @@
 #include "../C2/2-3/htoi.h"
 #include "gtest/gtest.h"
 
-TEST(SAMPLE, sample1){
-    char s[] = "0xff";
-    EXPECT_EQ(htoi(s), 255);
+TEST(C2, Q3){
+    char s0[] = "ff";
+    char s1[] = "0xff";
+    char s2[] = "0Xff";
+    char s3[] = "0xfF";
+    char s4[] = "0x10";
+    EXPECT_EQ(htoi(s0), 255);
+    EXPECT_EQ(htoi(s1), 255);
+    EXPECT_EQ(htoi(s2), 255);
+    EXPECT_EQ(htoi(s3), 255);
+    EXPECT_EQ(htoi(s4), 16);
 }
