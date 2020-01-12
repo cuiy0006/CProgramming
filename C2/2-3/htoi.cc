91aa048ba20c2dba46bddcdca5f01daaeb5d677c
cuiy0006 <yc2554@nyu.edu>
2020-01-12 02:59:58 -0500

    2-3

---
+++ C2/2-3/htoi.cc
@@ -0,0 +1,31 @@
+#include <ctype.h>
+
+int htoi(char* h){
+    int i = 0;
+    if(*h == '0' && (*(h + 1) == 'x' || *(h + 1) == 'X')){
+        i += 2;
+    }
+    int res = 0;
+    char c;
+    while((c = *(h + i)) != '\0'){
+        int tmp;
+        if(isdigit(c)){
+            tmp = c - '0';
+        } else {
+            if(c >= 'a' && c <= 'f'){
+                tmp = c - 'a' + 10;
+            } else {
+                tmp = c - 'A' + 10;
+            }
+        }
+        res = 16 * res + tmp;
+        ++i;
+    }
+    return res;
+}
+
+#include <stdio.h>
+int main(){
+    char c1[] = "0xff";
+    printf("%d", htoi(c1));
+}
