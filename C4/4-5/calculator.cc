7b5f8c7acbf8544f22554e6c83bfb5bd34f83286
cuiy0006 <yc2554@nyu.edu>
2020-01-24 00:56:21 -0500

    4-5

---
+++ C4/4-5/calculator.cc
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "config.h"
+
+#define MAXOP 100
+
+int getop(char []);
+void push(double);
+double pop(void);
+void print_top(void);
+void duplicate_top(void);
+void swap_top_two(void);
+
+main(){
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while((type = getop(s)) != EOF){
+        switch (type)
+        {
+        case '\n':
+            printf("\t%.8g\n", pop());
+
+        case NUMBER:
+            push(atof(s));
+            break;
+
+        case COMMAND:
+            if(strcmp(s, "+") == 0)
+                push(pop() + pop());
+            else if(strcmp(s, "*") == 0)
+                push(pop() * pop());
+            else if(strcmp(s, "-") == 0){
+                op2 = pop();
+                push(pop() - op2);
+            } else if(strcmp(s, "/") == 0){
+                op2 = pop();
+                if(op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+            } else if((strcmp(s, "%") == 0)){
+                op2 = pop();
+                if(op2 != 0.0)
+                    push(fmod(pop(), op2));
+                else
+                    printf("error: zero divisor\n");
+            } else if((strcmp(s, "ptop") == 0)){
+                print_top();
+            } else if((strcmp(s, "duptop") == 0)){
+                duplicate_top();
+            } else if((strcmp(s, "swaptop") == 0)){
+                swap_top_two();
+            } else if((strcmp(s, "sin") == 0)){
+                push(sin(pop()));
+            } else if((strcmp(s, "exp") == 0)){
+                push(exp(pop()));
+            } else if((strcmp(s, "pow") == 0)){
+                op2 = pop();
+                push(pow(pop(), op2));
+            }
+            else
+                printf("error: unknown command %s\n", s);
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+    return 0;
+}
---
+++ C4/4-5/getop.cc
@@ -0,0 +1,32 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "config.h"
+
+int getch(void);
+void ungetch(int c);
+
+int getop(char s[]){
+    int i, c, type;
+    while((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    if(c == '\n')
+        return '\n';
+    ungetch(c);
+    i = 0;
+    if(!isdigit(c) && c != '.'){
+        while((c = getch()) != ' ' && c != '\t' && c != '\n'){
+            s[i++] = c;
+        }
+        type = COMMAND;
+    } else {
+        while(isdigit(c = getch()) || c == '.'){
+            s[i++] = c;
+        }
+        type = NUMBER;
+    }
+
+    s[i] = '\0';
+    if(c != EOF)
+        ungetch(c);
+    return type;
+}
