6e118f94bea2217bd9cb010a3e8ccccfad5d9116
cuiy0006 <yc2554@nyu.edu>
2020-05-24 23:40:09 -0400

    1-18

--- C1/1-18/remove_trailing_space.cc
+++ C1/1-18/remove_trailing_space.cc
@@ -2,16 +2,22 @@
 
 int remove_trailing_space(char s[]){
     int i = 0;
+    bool newline = false;
     while(s[i] != '\0'){
         ++i;
     }
     --i;
-    --i;
+    if(s[i] == '\n'){
+        --i;
+        newline = true;
+    }
     while(i >= 0 && (s[i] == ' ' || s[i] == '\t')){
         --i;
     }
-    ++i;
-    s[i] = '\n';
+    if(newline){
+        ++i;
+        s[i] = '\n';
+    }
     ++i;
     s[i] = '\0';
     return i; 
